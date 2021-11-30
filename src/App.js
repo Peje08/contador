b1c8316b9d794db2af9cf5a8b547e7540795268d
Peje <guillepazerrobidart@yahoo.com.ar>
2021-11-29 22:13:33 -0300

    [MAKE NEW APP] cronometro

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [time, setTime] = useState(0);
+  const [timerOn, setTimerOn] = useState(false);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (timerOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 10);
+      }, 10);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [timerOn]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="numbers">
+        <span>{("0" + (Math.floor(time / 60000) % 60)).slice(-2)}:</span> 
+        <span>{("0" + (Math.floor(time / 1000) % 60)).slice(-2)}:</span>
+        <span>{("0" + ((time / 10) % 100)).slice(-2)}</span>
+      </div>
+
+      <div className="buttonContainer">
+        {!timerOn && time === 0 && (
+          <button className="btn" onClick={() => setTimerOn(true)}>START</button>
+        )}
+        {!timerOn &&(
+          <button className="btn" onClick={() => setTimerOn(true)}>RESUME</button>
+          )}
+        {timerOn && time !== 0 && (
+          <button className="btn" onClick={() => setTimerOn()}>STOP</button>
+        )}
+        {!timerOn && time > 0 && (
+          <button className="btn" onClick={() => setTime(0)}>RESET</button>
+        )}
+      </div>
     </div>
   );
 }
